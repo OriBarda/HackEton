2fc45fd1f6f744dc6999d897cd48d9a652c51afa
Ori Barda <oribarda10@gmail.com>
2024-02-28 16:24:36 +0200

    hellowroldaldalda

--- hack/src/main.jsx
+++ hack/src/main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { TeacherProvider } from "./Context/TeacherContext.jsx";
+import { StudentProvider } from "./Context/StudentContext.jsx";
+import { LessonProvider } from "./Context/LessonContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
+    <TeacherProvider>
+      <StudentProvider>
+        <LessonProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </LessonProvider>
+      </StudentProvider>
+    </TeacherProvider>
   </BrowserRouter>
-)
+);
