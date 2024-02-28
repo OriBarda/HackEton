e57c633288c7804567e723e73b4d983c9e52f006
Itay Sadras <itaysadr@gmail.com>
2024-02-28 17:29:33 +0200

    trying to commit lessonprovider

--- hack/src/main.jsx
+++ hack/src/main.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { TeacherProvider } from './Context/TeacherContext.jsx'
-import { StudentProvider } from './Context/StudentContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { TeacherProvider } from "./Context/TeacherContext.jsx";
+import { StudentProvider } from "./Context/StudentContext.jsx";
+import { LessonProvider } from "./Context/LessonContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <TeacherProvider>
       <StudentProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
+        <LessonProvider>
+          <React.StrictMode>
+            <App />
+          </React.StrictMode>
+        </LessonProvider>
       </StudentProvider>
     </TeacherProvider>
   </BrowserRouter>
-)
+);
